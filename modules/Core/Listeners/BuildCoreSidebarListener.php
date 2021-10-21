48f929ba914a21e4fc79fccf9edcb76a3932d060
quocdaijr <quocdaijr@gmail.com>
2021-10-21 15:19:16 +0700

    update

--- modules/Core/Listeners/BuildCoreSidebarListener.php
+++ modules/Core/Listeners/BuildCoreSidebarListener.php
@@ -22,11 +22,26 @@ class BuildCoreSidebarListener extends BuildSidebarListener
 </svg>');
                 $item->authorize(
                     $this->checkAccess((array)[
+                        'activity',
                         'horizon.index',
                         'telescope'
                     ])
                 );
                 $item->weight(10);
+                if (Route::has('activity')) {
+                    $item->item(__('Activity'), function (Item $item) {
+                        $item->icon('<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+</svg>');
+                        $item->route('activity');
+                        $item->isActiveWhen(route('activity', null, false));
+                        $item->authorize(
+                            $this->checkAccess((array)'activity')
+                        );
+                        $item->isNewTab(true);
+                    });
+                }
                 if (Route::has('horizon.index')) {
                     $item->item(__('Horizon'), function (Item $item) {
                         $item->icon('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" class="w-6 h-6" fill="currentColor">
--- resources/views/layouts/logger.blade.php
+++ resources/views/layouts/logger.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>@yield('title')</title>
+    <title>@yield('template_title')</title>
     <link
         href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
         rel="stylesheet"
@@ -12,7 +12,9 @@
 </head>
 <body>
 <nav class="navbar navbar-light sticky-top bg-primary flex-md-nowrap p-0 mb-3">
-    <a class="navbar-brand col-sm-3 col-md-2 mr-0 text-white" href="/"> <img class="mr-2" src="/favicon.ico">{{config('app.name')}}</a>
+    <a class="navbar-brand col-sm-3 col-md-2 mr-0 text-white" href="/">
+        <img class="mr-2" src="/favicon.ico">{{config('app.name')}}
+    </a>
 </nav>
 @yield('content')
 @stack('scripts')
