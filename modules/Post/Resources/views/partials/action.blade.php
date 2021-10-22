3c15b4eccfb02e32077b8599d12447484f3c59d0
quocdaijr <quocdaijr@gmail.com>
2021-10-22 16:57:19 +0700

    fix assets

--- modules/Post/Resources/views/partials/action.blade.php
+++ modules/Post/Resources/views/partials/action.blade.php
@@ -8,45 +8,45 @@ use Modules\Post\Constants\PostConstant;
     @switch($post->status ?? null)
         @case(\Modules\Post\Constants\PostConstant::STATUS_DRAFT)
             <button type="submit" data-action="publish" name="action" value="publish"
-                    class="w-24 px-5 py-3 font-medium font-bold leading-5 text-white transition-colors duration-150 bg-green-500 border border-transparent rounded-lg hover:bg-green-700">
+                    class="w-24 px-3 py-3 font-medium font-bold leading-5 text-white transition-colors duration-150 bg-green-500 border border-transparent rounded-lg hover:bg-green-700">
                 Publish
             </button>
             <button type="submit" data-action="save" name="action" value="save"
-                    class="w-24 px-5 py-3 font-medium font-bold leading-5 text-white transition-colors duration-150 bg-blue-500 border border-transparent rounded-lg hover:bg-blue-700">
+                    class="w-24 px-3 py-3 font-medium font-bold leading-5 text-white transition-colors duration-150 bg-blue-500 border border-transparent rounded-lg hover:bg-blue-700">
                 Save
             </button>
             <button type="submit" data-action="delete" name="action" value="delete"
-                    class="w-24 px-5 py-3 font-medium font-bold leading-5 text-white transition-colors duration-150 bg-red-500 border border-transparent rounded-lg hover:bg-red-700">
+                    class="w-24 px-3 py-3 font-medium font-bold leading-5 text-white transition-colors duration-150 bg-red-500 border border-transparent rounded-lg hover:bg-red-700">
                 Delete
             </button>
         @break
         @case(\Modules\Post\Constants\PostConstant::STATUS_PUBLISHED)
             <button type="submit" data-action="save" name="action" value="save"
-                    class="w-24 px-5 py-3 font-medium font-bold leading-5 text-white transition-colors duration-150 bg-blue-500 border border-transparent rounded-lg hover:bg-blue-700">
+                    class="w-24 px-3 py-3 font-medium font-bold leading-5 text-white transition-colors duration-150 bg-blue-500 border border-transparent rounded-lg hover:bg-blue-700">
                 Save
             </button>
             <button type="submit" data-action="delete" name="action" value="delete"
-                    class="w-24 px-5 py-3 font-medium font-bold leading-5 text-white transition-colors duration-150 bg-red-500 border border-transparent rounded-lg hover:bg-red-700">
+                    class="w-24 px-3 py-3 font-medium font-bold leading-5 text-white transition-colors duration-150 bg-red-500 border border-transparent rounded-lg hover:bg-red-700">
                 Delete
             </button>
         @break
         @case(\Modules\Post\Constants\PostConstant::STATUS_TRASH)
             <button type="submit" data-action="restore" name="action" value="restore"
-                    class="w-24 px-5 py-3 font-medium font-bold leading-5 text-white transition-colors duration-150 bg-yellow-500 border border-transparent rounded-lg hover:bg-yellow-700">
+                    class="w-24 px-3 py-3 font-medium font-bold leading-5 text-white transition-colors duration-150 bg-yellow-500 border border-transparent rounded-lg hover:bg-yellow-700">
                 Restore
             </button>
             <button type="submit" data-action="save" name="action" value="save"
-                    class="w-24 px-5 py-3 font-medium font-bold leading-5 text-white transition-colors duration-150 bg-blue-500 border border-transparent rounded-lg hover:bg-blue-700">
+                    class="w-24 px-3 py-3 font-medium font-bold leading-5 text-white transition-colors duration-150 bg-blue-500 border border-transparent rounded-lg hover:bg-blue-700">
                 Save
             </button>
         @break
         @default
             <button type="submit" data-action="publish" name="action" value="publish"
-                    class="w-24 px-5 py-3 font-medium font-bold leading-5 text-white transition-colors duration-150 bg-green-500 border border-transparent rounded-lg hover:bg-green-700">
+                    class="w-24 px-3 py-3 font-medium font-bold leading-5 text-white transition-colors duration-150 bg-green-500 border border-transparent rounded-lg hover:bg-green-700">
                 Publish
             </button>
             <button type="submit" data-action="save" name="action" value="save"
-                    class="w-24 px-5 py-3 font-medium font-bold leading-5 text-white transition-colors duration-150 bg-blue-500 border border-transparent rounded-lg hover:bg-blue-700">
+                    class="w-24 px-3 py-3 font-medium font-bold leading-5 text-white transition-colors duration-150 bg-blue-500 border border-transparent rounded-lg hover:bg-blue-700">
                 Save
             </button>
     @endswitch
